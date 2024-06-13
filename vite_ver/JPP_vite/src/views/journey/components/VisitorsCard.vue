46f096fb9c8eaa3dd18bac9cb8e428fc91d437c6
minjoo <betaa0528@gmail.com>
2024-06-13 19:54:32 +0900

    박민주/장소 저장 부분 수정 중/2024-06-13

--- vite_ver/JPP_vite/src/views/journey/components/VisitorsCard.vue
+++ vite_ver/JPP_vite/src/views/journey/components/VisitorsCard.vue
@@ -5,8 +5,8 @@
         <div class="pt-2 px-4 ps-lg-0 pe-xl-5">
           <!-- Title-->
           <div class="d-sm-flex flex-wrap justify-content-between align-items-center border-bottom">
-            <h2 class="h3 py-2 me-2 text-center text-sm-start">Your products<span
-                class="badge bg-faded-accent fs-sm text-body align-middle ms-2">5</span></h2>
+            <h2 class="h3 py-2 me-2 text-center text-sm-start">Your Saved Places<span
+                class="badge bg-faded-accent fs-sm text-body align-middle ms-2">{{ placeCount }}</span></h2>
             <div class="py-2">
               <div class="d-flex flex-nowrap align-items-center pb-3">
                 <label class="form-label fw-normal text-nowrap mb-0 me-2" for="sorting">Sort by:</label>
@@ -29,21 +29,23 @@
 
 <script setup>
 import Places from './Places.vue';
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useSimpleStore } from '@/store/journeyJSON.js';
 import { storeToRefs } from 'pinia';
 const { fetchPlaceList } = useSimpleStore();
 
 const placeList = ref('')
-
+const placeCount = ref(0)
 
 const getList = async () => {
   await fetchPlaceList(placeList)
-
-  // await console.log(placeList.value[0].name)
 }
+onMounted(() => {
+  getList()
+  console.log(placeList.length)
+  placeCount.value = placeList.length
+})
 
-getList()
 
 
 
